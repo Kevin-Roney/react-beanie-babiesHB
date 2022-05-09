8963dde42506735bd666aae8a06d63acc5d42652
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-09 15:59:22 -0700

    changed test parameters

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import BeaniesPage from './BeaniesPage';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<BeaniesPage />);
+  const linkElement = screen.getByText(/Current Page~/i);
   expect(linkElement).toBeInTheDocument();
 });
