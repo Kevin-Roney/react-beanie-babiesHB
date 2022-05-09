d59d72860bcb8c90f4567632c773399b1458e77a
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-09 16:36:54 -0700

    fixed bug where you can click past pages that actually exist

--- src/BeaniesPage.js
+++ src/BeaniesPage.js
@@ -26,9 +26,9 @@ function App() {
       <div className='buttons'>
         {/* on click, this button should decrement the page in state  */}
         {/* also, disable this button when you are on the first page */}
-        <button onClick={() => setPage(page - 1)}>Previous Page</button>
+        <button disabled={page <= 1} onClick={() => setPage(page - 1)}>Previous Page</button>
         {/* on click, this button should increment the page in state  */}
-        <button onClick={() => setPage(page + 1)}>Next Page</button>
+        <button disabled={page >= 53} onClick={() => setPage(page + 1)}>Next Page</button>
       </div>
       {/* pass the beanie babies into the BeaniesList component */}
       <BeaniesList beanieBabies={beanieBabies}/>
