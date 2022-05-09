a85fbc84b14ded95575937dbca40e12d92f8cc54
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-09 12:42:00 -0700

    added html elements to be rendered from BeanieBaby, all displaying as intended

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -4,6 +4,10 @@ export default function BeanieBaby({ beanieBaby }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    null
+    <Link to={`/beanies/${beanieBaby.id}`}>
+      <p>{beanieBaby.title}</p>
+      <p>{beanieBaby.birthday}</p>
+      <img src={beanieBaby.image}/>
+    </Link>
   );
 }
